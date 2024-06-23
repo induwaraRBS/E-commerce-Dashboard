8ce49d1b30b4f1c6e595e84581321f9eede50a18
InduwaraRBS <induwarakanchana567@gmail.com>
2024-06-23 12:12:28 +0530

    Second commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
-app.get('/',(req,resp)=>{
-        resp.send("app is working...");
-});
+const connectDB = async () => {
+  mongoose.connect("mongodb://localhost:27017/e-comm");
+  const prodcutSchema = new mongoose.Schema({});
+  const product = mongoose.model("products", prodcutSchema);
+  const data = await product.find();
+  console.warn(data);
+};
 
 app.listen(5000);
